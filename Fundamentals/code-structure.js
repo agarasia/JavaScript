0a2c5e79f7ca2d3f2ba1d7e7b91aadaa2730abec
agarasia <agarasia@asu.edu>
2024-05-07 14:46:29 -0700

    Lecture 2

---
+++ Fundamentals/code-structure.js
@@ -0,0 +1,11 @@
+alert("hello")
+// This is a single line comment
+alert("world")
+/*
+    This is a multi-line comment
+*/
+
+
+
+
+// Always add semi colon to end the line. Implicit semi-colon also works 'in most of the cases'.
