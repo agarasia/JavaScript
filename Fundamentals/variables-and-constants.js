335fffb5f7f519d4f7b094023b6e0cf3d0a34dd3
agarasia <agarasia@asu.edu>
2024-05-10 12:36:34 -0700

    vars-and-constants

---
+++ Fundamentals/variables-and-constants.js
@@ -0,0 +1,23 @@
+
+// Initialize 'message' variable using let keyword.
+let message;
+message = 'hello!';
+
+// Can initialize multiple variables of different types in the same line.
+let user = 'John', age = 25, message2 = 'hello';
+
+// OLD SCHOOL way to initialize a variable
+var oldSchool;
+
+// Valid ways to initialize variables
+let amarAkbar, TRUE, AMARAKBAR, amrapali21, $arG, _aums_variable;
+
+// Invalid ways to initialize variables
+// let 1a; // cannot start with a digit
+// let my-name; // hyphens '-' aren't allowed in the name
+
+// There is a list of reserved words, which cannot be used as variable names because they are used by the language itself.
+// For example: let, class, return, and function are reserved.
+// The code below gives a syntax error:
+// let let = 5; // can't name a variable "let", error!
+// let return = 5; // also can't name it "return", error!
---
+++ Fundamentals/vars-and-const-tasks.js
@@ -0,0 +1,9 @@
+// Task 1
+let admin, Name;
+Name = "John";
+admin = Name;
+console.log(admin);
+
+// Task 2
+let planetName = "earth";
+let currentVisitor = "Aum";
